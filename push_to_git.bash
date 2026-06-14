3953670782fbd7a9fc28bf67b2c88b48f49395f3
Arvie1290 <arviefaezaalauya3@gmail.com>
2026-06-14 12:23:48 +0700

    update

--- push_to_git.bash
+++ push_to_git.bash
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+echo "================================="
+echo "      AClient Git Updater"
+echo "================================="
+echo ""
+echo "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"
+
+read -p "Commit message: " msg
+
 git add .
-git commit -m "Update"
+
+git commit -m "$msg"
+
 git push
+
+echo
+echo "Upload selesai."
+read -n 1 -s -r -p "Tekan tombol apa saja untuk keluar..."
+echo
